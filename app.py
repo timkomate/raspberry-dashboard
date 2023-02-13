f7bd38ce4d6709d0f17ed18bc01c16b3ee86db2d
Mate Timko <timko.mate@gmail.com>
2023-02-13 21:08:53 +0000

    refactorization

--- app.py
+++ app.py
@@ -15,42 +15,45 @@ url_inside = "mariadb+mariadbconnector://root:my-secret-pw@0.0.0.0:3308/inside"
 url_outside = "mariadb+mariadbconnector://root:my-secret-pw@0.0.0.0:3308/outside"
 engine_inside = sqlalchemy.create_engine(url_inside)
 engine_outside = sqlalchemy.create_engine(url_outside)
-app = Dash(__name__, external_stylesheets = [dbc.themes.SOLAR])
+app = Dash(__name__, external_stylesheets=[dbc.themes.SOLAR])
 
-today = date.today() 
-tomorrow = today + timedelta(days = 1)
+today = date.today()
+tomorrow = today + timedelta(days=1)
 
-app.layout = html.Div([
-    html.H1(children = "RaspberryPi dashboard",
-        style={
-            'textAlign': 'center',
-        }
-    ),
-    dcc.DatePickerRange(
-        id='my-date-picker-range',
-        min_date_allowed=date(2019, 5, 1),
-        max_date_allowed=date(tomorrow.year, tomorrow.month, tomorrow.day),
-        initial_visible_month=date(today.year, today.month, today.day),
-        end_date=date(tomorrow.year, tomorrow.month, tomorrow.day),
-        start_date=date(today.year, today.month, today.day)
-    ),
-    html.Div(id='output-container-date-picker-range'),
-    dcc.Graph(id="graph-inside", figure = {}),
-    #dash_table(id="dash-table") 
+app.layout = html.Div(
+    [
+        html.H1(
+            children="RaspberryPi dashboard",
+            style={
+                "textAlign": "center",
+            },
+        ),
+        dcc.DatePickerRange(
+            id="my-date-picker-range",
+            min_date_allowed=date(2019, 5, 1),
+            max_date_allowed=date(tomorrow.year, tomorrow.month, tomorrow.day),
+            initial_visible_month=date(today.year, today.month, today.day),
+            end_date=date(tomorrow.year, tomorrow.month, tomorrow.day),
+            start_date=date(today.year, today.month, today.day),
+        ),
+        html.Div(id="output-container-date-picker-range"),
+        dcc.Graph(id="graph-inside", figure={}),
+        dash_table.DataTable(id="dash-table-inside"),
+        dash_table.DataTable(id="dash-table-outside")
     ]
-
 )
 
 
 @app.callback(
-Output('output-container-date-picker-range', 'children'),
-Output('graph-inside', 'figure'),
-Input('my-date-picker-range', 'start_date'),
-Input('my-date-picker-range', 'end_date'),
+    Output("output-container-date-picker-range", "children"),
+    Output("graph-inside", "figure"),
+#    Output("dash-table-inside", "children"),
+    Input("my-date-picker-range", "start_date"),
+    Input("my-date-picker-range", "end_date"),
 )
 def update_output(start_date, end_date):
     today = date.today()
-    tomorrow = today + timedelta(days = 1)
+    tomorrow = today + timedelta(days=1)
     start_date_object = date.fromisoformat(start_date)
     end_date_object = date.fromisoformat(end_date)
     sql = f"SELECT * FROM Data WHERE date BETWEEN '{start_date_object}' AND '{end_date_object}';"
@@ -58,22 +61,68 @@ def update_output(start_date, end_date):
         query = conn.execute(text(sql))
         print("Fetched from inside")
         df_inside = pd.DataFrame(query.fetchall())
-    with engine_outside.connect() as conn: 
+    with engine_outside.connect() as conn:
         query = conn.execute(text(sql))
         print("Fetched from outside")
         df_outside = pd.DataFrame(query.fetchall())
 
     fig = make_subplots(rows=4, cols=1, shared_xaxes=True)
-    fig.add_trace(go.Scatter(x=df_inside["date"], y=df_inside["temperature"],name="temperature inside"), row=1, col=1)
-    fig.add_trace(go.Scatter(x=df_outside["date"], y=df_outside["temperature"],name="temperature outside", visible = "legendonly"), row=1, col=1)
-    fig.add_trace(go.Scatter(x=df_inside["date"], y=df_inside["humidity"], name="humidity inside"), row=2, col=1)
-    fig.add_trace(go.Scatter(x=df_outside["date"], y=df_outside["humidity"], name="humidity outside", visible="legendonly"), row=2, col=1)
-    fig.add_trace(go.Scatter(x=df_outside["date"], y=df_outside["temperature"], name="temperature outside"), row=3, col=1)
-    fig.add_trace(go.Scatter(x=df_outside["date"], y=df_outside["humidity"], name="humidity outside"), row=4, col=1)
-    fig.update_layout(height=1090,
-            legend_title="Legend",)
-    return f"{start_date_object}-{end_date_object}",fig 
+    fig.add_trace(
+        go.Scatter(
+            x=df_inside["date"], y=df_inside["temperature"], name="temperature inside"
+        ),
+        row=1,
+        col=1,
+    )
+    fig.add_trace(
+        go.Scatter(
+            x=df_outside["date"],
+            y=df_outside["temperature"],
+            name="temperature outside",
+            visible="legendonly",
+        ),
+        row=1,
+        col=1,
+    )
+    fig.add_trace(
+        go.Scatter(
+            x=df_inside["date"], y=df_inside["humidity"], name="humidity inside"
+        ),
+        row=2,
+        col=1,
+    )
+    fig.add_trace(
+        go.Scatter(
+            x=df_outside["date"],
+            y=df_outside["humidity"],
+            name="humidity outside",
+            visible="legendonly",
+        ),
+        row=2,
+        col=1,
+    )
+    fig.add_trace(
+        go.Scatter(
+            x=df_outside["date"],
+            y=df_outside["temperature"],
+            name="temperature outside",
+        ),
+        row=3,
+        col=1,
+    )
+    fig.add_trace(
+        go.Scatter(
+            x=df_outside["date"], y=df_outside["humidity"], name="humidity outside"
+        ),
+        row=4,
+        col=1,
+    )
+    fig.update_layout(
+        height=1090,
+        legend_title="Legend",
+    )
+    return f"{start_date_object}-{end_date_object}", fig, #df_inside.to_dict()
 
 
 if __name__ == "__main__":
-    app.run_server(debug=True, host='0.0.0.0')
+    app.run_server(debug=True, host="0.0.0.0")
